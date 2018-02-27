48988c77f8ae6357e09912801962bf2de6cf9ab9
JC <j0shua.chen@yahoo.com>
2018-02-27 05:17:49 -0800

    create movie reducer/rating reducer

---
+++ frontend/reducers/movie_reducer.js
@@ -0,0 +1,38 @@
+import merge from 'lodash/merge';
+import {
+  RECEIVE_MOVIE,
+  RECEIVE_MOVIES ,
+  UPDATE_MOVIE,
+  RECEIVE_ERRORS
+} from '../actions/movie_actions.js';
+
+const initialState = Object.freeze({
+  errors: [],
+});
+
+const MovieReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let newState = merge({}, state);
+
+  switch(action.type){
+    case RECEIVE_MOVIES:
+    console.log(action);
+      return action.movies;
+    case RECEIVE_MOVIE:
+    console.log(action.movie);
+    return action.movie;
+      // const newMovie= {[action.movie.id]: action.movie};
+      // return newMovie;
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      console.log(action);
+
+      return merge({}, state, {
+        errors
+      });
+    default:
+    return state;
+  }
+};
+
+export default MovieReducer;
---
+++ frontend/reducers/rating_reducer.js
@@ -0,0 +1,41 @@
+import merge from 'lodash/merge';
+import {
+  RECEIVE_RATING,
+  RECEIVE_RATINGS ,
+  UPDATE_RATING,
+  RECEIVE_ERRORS
+} from '../actions/rating_actions.js';
+
+const initialState = Object.freeze({
+  errors: [],
+});
+
+const RatingReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let newState = merge({}, state);
+
+  switch(action.type){
+    case RECEIVE_RATINGS:
+    console.log(action);
+      return action.ratings;
+    case RECEIVE_RATING:
+    console.log(action);
+    // console.log(action.rating);
+    return action.rating;
+      // const newRating= {[action.rating.id]: action.rating};
+      // return newRating;
+    case UPDATE_RATING:
+      newState = merge({}, state);
+      newState[action.rating.id] = action.rating;
+      return newState;
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      return merge({}, state, {
+        errors
+      });
+    default:
+    return state;
+  }
+};
+
+export default RatingReducer;
